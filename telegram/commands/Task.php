aba11cee7f29f0996f2a65b256ce36148c7a9004
Александр Касьянов <info@carono.ru>
2024-01-09 15:15:05 +0500

    refs

--- telegram/commands/Task.php
+++ telegram/commands/Task.php
@@ -2,9 +2,11 @@
 
 namespace app\telegram\commands;
 
+use app\exceptions\ValidationException;
 use carono\telegram\Bot;
 use Exception;
 use Yii;
+use yii\db\Expression;
 use yii\helpers\ArrayHelper;
 
 class Task extends Command
@@ -14,6 +16,7 @@ class Task extends Command
     {
         $bot->hear('/add', [$this, 'actionAdd']);
         $bot->hear('/tasks', [$this, 'actionList']);
+        $bot->hear('/close', [$this, 'actionClose']);
         // TODO: Implement register() method.
     }
 
@@ -33,8 +36,27 @@ class Task extends Command
     public function actionList(\app\components\Bot $bot)
     {
         if ($user = $this->getUser($bot)) {
-            $titles = $user->getTasks()->andWhere(['finished_at' => null])->select(['title'])->column();
-            $bot->sayPrivate(implode("\r\n* ", $titles));
+            $titles = $user->getTasks()->andWhere(['finished_at' => null])->all();
+            $titles = array_map(function (\app\models\Task $task) {
+                return '* ' . trim($task->title) . ' (/close' . $task->id . ')';
+            }, $titles);
+            $bot->sayPrivate(implode("\r\n", $titles));
+        }
+    }
+
+    public function actionClose(\app\components\Bot $bot)
+    {
+        if (!$user = $this->getUser($bot)) {
+            return;
+        }
+        $id = (int)mb_substr($bot->message->text, 6);
+        if ($task = $user->getTasks()->andWhere(['finished_at' => null])->andWhere(['id' => $id])->one()) {
+            $task->finished_at = new Expression('NOW()');
+            if (!$task->save()) {
+                throw new ValidationException($task);
+            }
+            $bot->sayPrivate('Закрыли ' . $task->title);
+            $this->actionList($bot);
         }
     }
 }
