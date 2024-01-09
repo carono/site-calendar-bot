a41c37c559bbeec635e374f7b2bc75e2a41f48f1
Александр Касьянов <info@carono.ru>
2024-01-09 12:16:36 +0500

    refs

--- telegram/commands/Task.php
+++ telegram/commands/Task.php
@@ -4,6 +4,7 @@ namespace app\telegram\commands;
 
 use carono\telegram\Bot;
 use Yii;
+use yii\helpers\ArrayHelper;
 
 class Task extends Command
 {
@@ -11,6 +12,7 @@ class Task extends Command
     public function register(Bot $bot)
     {
         $bot->hear('/add', [$this, 'actionAdd']);
+        $bot->hear('/tasks', [$this, 'actionList']);
         // TODO: Implement register() method.
     }
 
@@ -21,6 +23,14 @@ class Task extends Command
             if ($task = \app\models\Task::add($bot->message, $user)) {
                 $bot->sayPrivate('Задачу создали: ' . $task->title);
             }
+        }
+    }
+
+    public function actionList(\app\components\Bot $bot)
+    {
+        if ($user = $this->getUser($bot)) {
+            $titles = $user->getTasks()->andWhere(['finished_at' => null])->select(['title'])->column();
+            $bot->sayPrivate(implode("\r\n* ", $titles));
         }
     }
 }
