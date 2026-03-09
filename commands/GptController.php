1547e5ee42e7b70c2fd41924b5bc056797ae9df0
Касьянов <info@carono.ru>
2026-03-09 15:40:15 +0500

    refs

--- commands/GptController.php
+++ commands/GptController.php
@@ -18,40 +18,7 @@ class GptController extends Controller
 {
     public function actionTest()
     {
-        $user = User::findOne(1);
-        $tasks = $user->getActiveTasks();
-        $groups = $user->getActiveGroups();
-//        $question = 'Запланируй починить девятку в пятницу';
-        $question = 'Найди задачу про починку девятки';
-        $response = AIHelper::start()
-            ->addSystem(new DateSystem())
-            ->addSystem(new FormatSystem())
-            ->addSystem(new DetermineSystem())
-            ->addSystem(new GroupsSystem(['data' => $groups]))
-            ->addSystem(new ActiveTaskSystem(['data' => $tasks]))
-            ->determine($question);
 
-        print_r($response);
 
-    }
-
-    public function actionAdd()
-    {
-        $user = User::findOne(1);
-        Task::add('Заехать завтра в налоговую', $user);
-
-    }
-
-    public function actionGrouping()
-    {
-//        $user = User::findOne(1);
-//        $tasks = $user->getTasks()->notFinished()->all();
-//        $response = AIHelper::grouping($user, $tasks);
-//        foreach ($response as $groupingDTO) {
-//            $task = $user->getTasks()->notFinished()->andWhere(['id' => $groupingDTO->task_id])->one();
-//            Console::output("{$groupingDTO->title} - {$groupingDTO->group}");
-//            $group = Group::findByName($user, $groupingDTO->group);
-//            $task->updateAttributes(['group_id' => $group->id]);
-//        }
     }
 }
